767dc6d5bae812c9d22762df55517d8d6755d90b
Daniel Floriano Santos <danielfloriano808@gmail.com>
2022-08-27 21:17:01 -0300

    exercicio 26

--- exercicios/ex026/mq005/js/index.js
+++ exercicios/ex026/mq005/js/index.js
@@ -6,3 +6,11 @@ function clickMenu() {
         menu.style.display = "block"
     }
 }
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
